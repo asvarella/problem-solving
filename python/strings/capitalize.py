0346eb51f256479b6e840442b400730174d930f5
asvarella <alice.varella@gmail.com>
2022-04-11 14:10:57 -0300

    commented solution

--- python/strings/capitalize.py
+++ python/strings/capitalize.py
@@ -9,7 +9,7 @@ import sys
 # Complete the solve function below.
 def solve(s):
     for i in s.split():
-        s = s.replace(i,i.capitalize())
+        s = s.replace(i,i.capitalize()) #this way you avoid losing extra spaces between words
     return s
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
