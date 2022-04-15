6ee73c096026820257c80b5736afbaa8c203281b
asvarella <alice.varella@gmail.com>
2022-04-14 22:53:34 -0300

    more essential set functions

---
+++ python/sets/set-intersection.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    narr = set(map(int, input().split()))
+    b = int(input())
+    barr = set(map(int, input().split()))
+    print(len(narr.intersection(barr)))
